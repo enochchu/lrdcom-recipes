fbc446f17dece3b06ac941f35556c24db5f3be18
ryanschuhler <ryan.schuhler@liferay.com>
2013-07-05 09:11:38 -0700

    lazy-load initial code

---
+++ lazy-load/README.md
@@ -0,0 +1,8 @@
+Lazy Load
+======
+
+In order to use lazy load all you need to do is add the class "lazy-load" to whatever element you desire. If you want to lazy load an image add the class and instead of placeing the img url into "src", place it in an attribute called "datasrc".
+
+Lazy load will happen when the div reaches the bottome of the visible screen. It only happens once, so once it is loaded the class and loaded image will stay there until refresh.
+
+[See it in action here](http://htmlpreview.github.io/?https://github.com/enochchu/lrdcom-recipes/blob/master/lazy-load/lazy_load.html)
---
+++ lazy-load/lazy_load.js
@@ -0,0 +1,46 @@
+// AUI().use(
+YUI().use(
+	'aui-base',
+	function(A) {
+		var WIN = A.getWin();
+
+		var lazyLoadNode = A.all('.lazy-load');
+
+		var lazyLoad = function() {
+			var currentScrollPos = WIN.get('docScrollY');
+
+			var winHeight = WIN.get('innerHeight');
+
+			// code to support ie 7 and 8
+			// if (winHeight == undefined) {
+			// 	winHeight = document.documentElement.­clientHeight;
+			// }
+
+			lazyLoadNode.each(
+				function(item, index, collection) {
+					if (!item.hasClass('lazy-loaded')) {
+						var loadPos = item.getY() - winHeight;
+
+						if (currentScrollPos > loadPos) {
+							var datasrc = item.attr('datasrc');
+							var src = item.attr('src');
+
+							if (src != datasrc) {
+								item.attr('src', datasrc);
+							}
+
+							item.addClass('lazy-loaded');
+						}
+					}
+				}
+			);
+		};
+
+		if (!lazyLoadNode.isEmpty()) {
+			A.on('scroll', lazyLoad);
+			A.on('resize', lazyLoad);
+
+			lazyLoad();
+		}
+	}
+);
